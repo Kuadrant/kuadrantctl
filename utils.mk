052f34f77e21f8ef87ca839457d3498428de8a2f
Dmitry Volodin <dmvolod@gmail.com>
2021-07-23 10:58:55 +0300

    Utilize go install for kind downloading

--- utils.mk
+++ utils.mk
@@ -2,11 +2,7 @@
 define go-get-tool
 @[ -f $(1) ] || { \
 set -e ;\
-TMP_DIR=$$(mktemp -d) ;\
-cd $$TMP_DIR ;\
-go mod init tmp ;\
 echo "Downloading $(2)" ;\
-GOBIN=$(PROJECT_PATH)/bin go get $(2) ;\
-rm -rf $$TMP_DIR ;\
+GOBIN=$(PROJECT_PATH)/bin go install $(2) ;\
 }
 endef
