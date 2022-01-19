77c19a5657b913e476e8ba520f48149102e3c443
Eguzki Astiz Lezaun <eastizle@redhat.com>
2022-01-19 17:20:02 +0100

    remove unused istio.io/client-go/pkg/apis/security/v1beta1 API

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.13.0
 	github.com/spf13/cobra v1.2.1
-	istio.io/client-go v1.10.1
+	istio.io/client-go v1.12.1
 	k8s.io/api v0.21.2
 	k8s.io/apiextensions-apiserver v0.21.2
 	k8s.io/apimachinery v0.21.2
--- pkg/utils/scheme.go
+++ pkg/utils/scheme.go
@@ -5,7 +5,6 @@ import (
 	networkingv1beta1 "github.com/kuadrant/kuadrant-controller/apis/networking/v1beta1"
 	limitadorv1alpha1 "github.com/kuadrant/limitador-operator/api/v1alpha1"
 	istio "istio.io/client-go/pkg/apis/networking/v1alpha3"
-	istioSecurity "istio.io/client-go/pkg/apis/security/v1beta1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiextensionsv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -23,11 +22,6 @@ func SetupScheme() error {
 	}
 
 	err = istio.AddToScheme(scheme.Scheme)
-	if err != nil {
-		return err
-	}
-
-	err = istioSecurity.AddToScheme(scheme.Scheme)
 	if err != nil {
 		return err
 	}
