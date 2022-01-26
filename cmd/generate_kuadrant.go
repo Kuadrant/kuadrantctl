b35b52e185ef2a23e408ffe24813b4f2623ca0f2
Eguzki Astiz Lezaun <eastizle@redhat.com>
2022-01-26 19:09:38 +0100

    Update cmd/generate_kuadrant.go
    Co-authored-by: Guilherme Cassolato <guicassolato@gmail.com>

--- cmd/generate_kuadrant.go
+++ cmd/generate_kuadrant.go
@@ -8,7 +8,7 @@ func generateKuadrantCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "kuadrant",
 		Short: "Generate Kuadrant resources",
-		Long:  "Generate Kuadrant resorces",
+		Long:  "Generate Kuadrant resources",
 	}
 
 	cmd.AddCommand(generateKuadrantAuthconfigCommand())
