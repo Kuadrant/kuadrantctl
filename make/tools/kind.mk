815be5ffd73a6d5e8d9a509db0b51de8a03883d9
Eguzki Astiz Lezaun <eastizle@redhat.com>
2024-06-28 10:39:12 +0200

    makefile enhancements

---
+++ make/tools/kind.mk
@@ -0,0 +1,7 @@
+KIND = $(PROJECT_PATH)/bin/kind
+KIND_VERSION = v0.22.0
+$(KIND):
+	$(call go-install-tool,$(KIND),sigs.k8s.io/kind@$(KIND_VERSION))
+
+.PHONY: kind
+kind: $(KIND) ## Download kind locally if necessary.
---
+++ make/tools/kustomize.mk
@@ -0,0 +1,6 @@
+KUSTOMIZE = $(PROJECT_PATH)/bin/kustomize
+$(KUSTOMIZE):
+	$(call go-install-tool,$(KUSTOMIZE),sigs.k8s.io/kustomize/kustomize/v4@v4.5.5)
+
+.PHONY: kustomize
+kustomize: $(KUSTOMIZE) ## Download kustomize locally if necessary.
